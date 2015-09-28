9a0703586621702802b94318e9bb75a16969f67c
Paullius <p.paulius@gmail.com>
2015-09-27 20:33:30 -0700

    undirected breadth first draft

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 
  func main() {
  	var g undirected.Graph = undirected.CreateGraph(5)
+ 	undirected.AddEdge(1,2,g)
  	var dfs undirected.DepthFirst = undirected.DepthFirstSearch(g,2)
  	var dfps undirected.DepthFirstPaths = undirected.DepthFirstPathsSearch(g,2)
+ 	var bfps undirected.BreadthFirstPaths = undirected.BreadthFirstPathsSearch(g,2)
 
  	fmt.Println(g.V)
  	fmt.Println(dfs.Count)
  	fmt.Println(dfps.S)
+ 	fmt.Println(bfps.S)
 }
 
 //go install github.com/paullius/go-graphs-
