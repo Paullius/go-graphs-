bc03f4931edbdc7f35721efe739cdcc436a73eba
Paullius <p.paulius@gmail.com>
2015-09-27 18:18:29 -0700

    dfps example

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
  func main() {
  	var g undirected.Graph = undirected.CreateGraph(5)
  	var dfs undirected.DepthFirst = undirected.DepthFirstSearch(g,2)
+ 	var dfps undirected.DepthFirstPaths = undirected.DepthFirstPathsSearch(g,2)
 
  	fmt.Println(g.V)
  	fmt.Println(dfs.Count)
+ 	fmt.Println(dfps.S)
 }
 
 //go install github.com/paullius/go-graphs-
