27a25650d59f1805c724845e44e2be21b9a9ed37
Paullius <p.paulius@gmail.com>
2015-09-28 10:24:36 -0700

    moved queue to separate file

---
+++ undirected/queue.go
@@ -0,0 +1,17 @@
+package undirected
+
+type Queue []int
+
+func (q *Queue) Push(n int) {
+	*q = append(*q, n)
+}
+
+func (q *Queue) Pop() (n int) {
+	n = (*q)[0]
+	*q = (*q)[1:]
+	return
+}
+
+func (q *Queue) Len() int {
+	return len(*q)
+}
