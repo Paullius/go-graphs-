9a577a7d09dc5a5ea48db685cecfec509f9797e8
Paullius <p.paulius@gmail.com>
2015-12-20 16:00:07 -0800

    web service example

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/paullius/go-graphs-/nonWeighted/directed/examples"
 	"github.com/paullius/go-graphs-/nonWeighted/undirected/examples"
 	"github.com/paullius/go-graphs-/weighted/minimumSpanningTree/examples"
 	"github.com/paullius/go-graphs-/weighted/shortestPaths/examples"
+	"log"
+	"net/http"
 	"sync"
 )
 
@@ -19,13 +22,37 @@ func main() {
 
 	minimumSpanningTree.PrimMstExample()
 
+	shortestPaths.DijkstrasExample(0, []int{1, 2, 3, 4, 5, 6, 7})
+
+	asyncRun()
+
+	startWebServer()
+
+}
+
+func asyncRun() {
 	var wg sync.WaitGroup
-	wg.Add(1)
+	wg.Add(2)
+	go func() {
+		minimumSpanningTree.PrimMstExample()
+		defer wg.Done()
+	}()
 	go func() {
 		shortestPaths.DijkstrasExample(0, []int{1, 2, 3, 4, 5, 6, 7})
 		defer wg.Done()
 	}()
 	wg.Wait()
+}
+
+func startWebServer() {
+	http.HandleFunc("/hello", handleHello)
+	fmt.Println("Server on http://localhost:7777/hello")
+	log.Fatal(http.ListenAndServe("localhost:7777", nil))
+}
+
+func handleHello(w http.ResponseWriter, req *http.Request) {
+	log.Println("serving", req.URL)
+	fmt.Fprintln(w, "<html>Hello, <b>User</b>:)<html>")
 }
 
 //go run %GOPATH%\src\github.com\Paullius\go-graphs-\main.go
